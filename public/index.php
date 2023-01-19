d43e210a386695cf264872e873fde26181490d8f
Kirsi Nykänen <kirsi.nykanen@edu.sasky.fi>
2023-01-19 10:46:46 +0200

    config-määrityksille oma tiedosto

--- public/index.php
+++ public/index.php
@@ -1,19 +1,22 @@
 <?php
+// Suoritetaan projektin alustusskripti.
+require_once '../src/init.php';
 
-  // Siistitään polku urlin alusta ja mahdolliset parametrit urlin lopusta.
-  // Siistimisen jälkeen osoite /~knykanen/lanify/tapahtuma?id=1 on 
-  // lyhentynyt muotoon /tapahtuma.
-  $request = str_replace('/~knykanen/lanify','',$_SERVER['REQUEST_URI']);
-  $request = strtok($request, '?');
+// Siistitään polku urlin alusta ja mahdolliset parametrit urlin lopusta.
+// Siistimisen jälkeen osoite /~knykanen/lanify/tapahtuma?id=1 on 
+// lyhentynyt muotoon /tapahtuma.
+$request = str_replace($config['urls']['baseUrl'],'',$_SERVER['REQUEST_URI']);
 
-  // Selvitetään mitä sivua on kutsuttu ja suoritetaan sivua vastaava 
-  // käsittelijä.
-  if ($request === '/' || $request === '/tapahtumat') {
-    echo '<h1>Kaikki tapahtumat</h1>';
-  } else if ($request === '/tapahtuma') {
-    echo '<h1>Yksittäisen tapahtuman tiedot</h1>';
-  } else {
-    echo '<h1>Pyydettyä sivua ei löytynyt :(</h1>';
-  }
+$request = strtok($request, '?');
+
+// Selvitetään mitä sivua on kutsuttu ja suoritetaan sivua vastaava 
+// käsittelijä.
+if ($request === '/' || $request === '/tapahtumat') {
+  echo '<h1>Kaikki tapahtumat</h1>';
+} else if ($request === '/tapahtuma') {
+  echo '<h1>Yksittäisen tapahtuman tiedot</h1>';
+} else {
+  echo '<h1>Pyydettyä sivua ei löytynyt :(</h1>';
+}
 
 ?> 
