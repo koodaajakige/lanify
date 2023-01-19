9cdbc1f0e0d61d7aee75a603c27f14dd3b6c5438
Kirsi Nykänen <kirsi.nykanen@edu.sasky.fi>
2023-01-19 13:10:27 +0200

    Hakemistoviittaukset vakioiksi

--- config/config.php
+++ config/config.php
@@ -7,3 +7,9 @@
   );
 
 ?>
+
+  define("PROJECT_ROOT", dirname(__DIR__) . "/");
+  define("HELPERS_DIR", PROJECT_ROOT . "src/helpers/");
+  define("TEMPLATE_DIR", PROJECT_ROOT . "src/view/");
+  define("MODEL_DIR", PROJECT_ROOT . "src/model/");
+  define("CONTROLLER_DIR", PROJECT_ROOT . "src/controller/");
