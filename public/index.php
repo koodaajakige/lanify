6c479fe647ce614cfbdaa01407600491fcb58ce9
Kirsi Nykänen <kirsi.nykanen@edu.sasky.fi>
2023-01-19 12:42:34 +0200

    Sivupohjat lisätty

--- public/index.php
+++ public/index.php
@@ -1,22 +1,25 @@
-<?php
-// Suoritetaan projektin alustusskripti.
-require_once '../src/init.php';
+  <?php
+  // Suoritetaan projektin alustusskripti.
+  require_once '../src/init.php';
 
-// Siistitään polku urlin alusta ja mahdolliset parametrit urlin lopusta.
-// Siistimisen jälkeen osoite /~knykanen/lanify/tapahtuma?id=1 on 
-// lyhentynyt muotoon /tapahtuma.
-$request = str_replace($config['urls']['baseUrl'],'',$_SERVER['REQUEST_URI']);
+  // Siistitään polku urlin alusta ja mahdolliset parametrit urlin lopusta.
+  // Siistimisen jälkeen osoite /~knykanen/lanify/tapahtuma?id=1 on 
+  // lyhentynyt muotoon /tapahtuma.
+  $request = str_replace($config['urls']['baseUrl'],'',$_SERVER['REQUEST_URI']);
 
-$request = strtok($request, '?');
+  $request = strtok($request, '?');
 
-// Selvitetään mitä sivua on kutsuttu ja suoritetaan sivua vastaava 
-// käsittelijä.
-if ($request === '/' || $request === '/tapahtumat') {
-  echo '<h1>Kaikki tapahtumat</h1>';
-} else if ($request === '/tapahtuma') {
-  echo '<h1>Yksittäisen tapahtuman tiedot</h1>';
-} else {
-  echo '<h1>Pyydettyä sivua ei löytynyt :(</h1>';
-}
+  // Luodaan uusi Plates-olio ja kytketään se sovelluksen sivupohjiin.
+  $templates = new League\Plates\Engine('../src/view');
+
+  // Selvitetään mitä sivua on kutsuttu ja suoritetaan sivua vastaava
+  // käsittelijä.
+  if ($request === '/' || $request === '/tapahtumat') {
+    echo $templates->render('tapahtumat');
+  } else if ($request === '/tapahtuma') {
+    echo $templates->render('tapahtuma');
+  } else {
+    echo $templates->render('notfound');
+  }
 
 ?> 
