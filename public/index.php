471269c6423248d5cf2e4ad89b2ffcab4c72bc8f
Kirsi Nykänen <kirsi.nykanen@edu.sasky.fi>
2023-02-09 10:25:01 +0200

    Istunnon suojaaminen

--- public/index.php
+++ public/index.php
@@ -52,10 +52,11 @@
     case "/kirjaudu":
       if (isset($_POST['laheta'])) {
         require_once CONTROLLER_DIR . 'kirjaudu.php';
-      if (tarkistaKirjautuminen($_POST['email'],$_POST['salasana'])) {
-        $_SESSION['user'] = $_POST['email'];
-        header("Location: " . $config['urls']['baseUrl']);
-      } else {
+        if (tarkistaKirjautuminen($_POST['email'],$_POST['salasana'])) {
+          session_regenerate_id();
+          $_SESSION['user'] = $_POST['email'];
+          header("Location: " . $config['urls']['baseUrl']);
+        } else {
         echo $templates->render('kirjaudu', [ 'error' => ['virhe' => 'Väärä käyttäjätunnus tai salasana!']]);
       }
     } else {
