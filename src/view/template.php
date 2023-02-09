e476db740ec03ac3a9f13d90cadd34df1556917c
Kirsi Nykänen <kirsi.nykanen@edu.sasky.fi>
2023-02-09 10:13:50 +0200

    Kirjautumistietojen tulostus

--- src/view/template.php
+++ src/view/template.php
@@ -6,8 +6,18 @@
     <link href="styles/styles.css" rel="stylesheet">   
   </head>
   <body>
-    <header>
+  <header>
       <h1><a href="<?=BASEURL?>">lanify</a></h1>
+      <div class="profile">
+        <?php
+          if (isset($_SESSION['user'])) {
+            echo "<div>$_SESSION[user]</div>";
+            echo "<div><a href='logout'>Kirjaudu ulos</a></div>";
+          } else {
+            echo "<div><a href='kirjaudu'>Kirjaudu</a></div>";
+          }
+        ?>
+      </div>
     </header>
     <section>
       <?=$this->section('content')?>
