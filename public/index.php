c63456edf6356052d925deb55ac8472659a8b6a0
Kirsi Nykänen <kirsi.nykanen@edu.sasky.fi>
2023-02-09 10:19:20 +0200

    Uloskirjautuminen

--- public/index.php
+++ public/index.php
@@ -34,36 +34,39 @@
         echo $templates->render('tapahtumanotfound');
       }
       break;
-      case '/lisaa_tili':
-        if (isset($_POST['laheta'])) {
-          $formdata = cleanArrayData($_POST);
-          require_once CONTROLLER_DIR . 'tili.php';
-          $tulos = lisaaTili($formdata);
-          if ($tulos['status'] == "200") {
-            echo $templates->render('tili_luotu', ['formdata' => $formdata]);
-            break;
-          }
-          echo $templates->render('lisaa_tili', ['formdata' => $formdata, 'error' => $tulos['error']]);
+    case '/lisaa_tili':
+      if (isset($_POST['laheta'])) {
+        $formdata = cleanArrayData($_POST);
+        require_once CONTROLLER_DIR . 'tili.php';
+        $tulos = lisaaTili($formdata);
+        if ($tulos['status'] == "200") {
+          echo $templates->render('tili_luotu', ['formdata' => $formdata]);
           break;
-        } else {
-          echo $templates->render('lisaa_tili', ['formdata' => [], 'error' => []]);
-          break;
-        } 
-      case "/kirjaudu":
-        if (isset($_POST['laheta'])) {
-          require_once CONTROLLER_DIR . 'kirjaudu.php';
-        if (tarkistaKirjautuminen($_POST['email'],$_POST['salasana'])) {
-          $_SESSION['user'] = $_POST['email'];
-          header("Location: " . $config['urls']['baseUrl']);
-
-        } else {
-          echo $templates->render('kirjaudu', [ 'error' => ['virhe' => 'Väärä käyttäjätunnus tai salasana!']]);
         }
+        echo $templates->render('lisaa_tili', ['formdata' => $formdata, 'error' => $tulos['error']]);
+        break;
       } else {
-        echo $templates->render('kirjaudu', [ 'error' => []]);
+        echo $templates->render('lisaa_tili', ['formdata' => [], 'error' => []]);
+        break;
+      } 
+    case "/kirjaudu":
+      if (isset($_POST['laheta'])) {
+        require_once CONTROLLER_DIR . 'kirjaudu.php';
+      if (tarkistaKirjautuminen($_POST['email'],$_POST['salasana'])) {
+        $_SESSION['user'] = $_POST['email'];
+        header("Location: " . $config['urls']['baseUrl']);
+      } else {
+        echo $templates->render('kirjaudu', [ 'error' => ['virhe' => 'Väärä käyttäjätunnus tai salasana!']]);
       }
+    } else {
+      echo $templates->render('kirjaudu', [ 'error' => []]);
+    }
+    break;
+    case "/logout":
+      require_once CONTROLLER_DIR . 'kirjaudu.php';
+      logout();
+      header("Location: " . $config['urls']['baseUrl']);
       break;
-    
     default:
       echo $templates->render('notfound');
   }
--- src/controller/kirjaudu.php
+++ src/controller/kirjaudu.php
@@ -15,7 +15,27 @@
 
     // Käyttäjää ei löytynyt tai salasana oli väärin. 
     return false;
+  }
+  
+
+  function logout() {
+
+    // Tyhjennetään istuntomuuttujat.
+    $_SESSION = array();
+
+    // Poistetaan istunnon eväste.
+    if (ini_get("session.use_cookies")) {
+      $params = session_get_cookie_params();
+      setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+      );
+    }
+
+    // Tuhotaan vielä lopuksi istunto.
+    session_destroy();
 
   }
+
 
 ?>
