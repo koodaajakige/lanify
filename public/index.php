39cf1c9b7ed358f0de46e605f9820dd6a303832b
Kirsi Nykänen <kirsi.nykanen@edu.sasky.fi>
2023-02-15 19:33:09 +0200

    Vahvistuslinkin aktivointi

--- public/index.php
+++ public/index.php
@@ -108,6 +108,20 @@
         header("Location: tapahtumat");  
       }
       break;
+      case "/vahvista":
+        if (isset($_GET['key'])) {
+          $key = $_GET['key'];
+          require_once MODEL_DIR . 'henkilo.php';
+          if (vahvistaTili($key)) {
+            echo $templates->render('tili_aktivoitu');
+          } else {
+            echo $templates->render('tili_aktivointi_virhe');
+          }
+        } else {
+          header("Location: " . $config['urls']['baseUrl']);
+        }
+        break;
+  
     default:
       echo $templates->render('notfound');
   }
